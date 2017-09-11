9ad6f03a5fb06983b3f329b83bc47ce5b2a1fed4
Matheus Prado Rodrigues <matheusprador@gmail.com>
2017-09-11 17:11:42 -0300

    First commit of codes

---
+++ controllers/HomeController.php
@@ -0,0 +1,1 @@
+<?phpclass HomeController extends Controller{    public function __construct()    {        parent::__construct();        $this->assets->site();    }    public function actionIndex()    {        $this->renderHtml("home");    }}
