c97c260c6f8513e3e5880948c6354e14e5425337
Matheus Prado Rodrigues <matheusprador@gmail.com>
2017-09-12 17:13:22 -0300

    Add index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+#Don't erase this line <Winged>#
+define("DOCUMENT_ROOT", str_replace("\\", "/", dirname(__FILE__) . "/"));
+include_once "./winged/classes/winged.class.php";
+Winged::start();
