7a245c2d622ac813525efa25299d7722c1bdd732
Matheus Prado Rodrigues <matheusprador@gmail.com>
2019-04-02 16:34:06 -0300

    Updates in config.php

--- config.php
+++ config.php
@@ -9,41 +9,43 @@ namespace Winged;
  * project globally at a later time. You can also override the properties
  * within other config.php files. These files must be created inside
  * other directories if you want to overwrite some properties at runtime.
- *
- * For questions about the framework, enable the $SEE_SYSTEM_PAGE option in config.php
- * from the root to true and after enter the link <your_domain_name>/winged/api/
  */
-class WingedConfig extends \stdClass
+class WingedConfig extends WingedConfigDefaults
 {
+    /**
+     * @var null | WingedConfig
+     * no delete this property
+     */
+    public static $config = null;
     /**
      * @property $MAIN_CONTENT_TYPE string
      * set content type in header
      */
-    public static $MAIN_CONTENT_TYPE = "text/html";
+    public $MAIN_CONTENT_TYPE = "text/html";
 
     /**
      * @property $HTML_CHARSET string
      * set charset for content input, output html and internal encoding
      */
-    public static $HTML_CHARSET = "UTF-8";
+    public $HTML_CHARSET = "UTF-8";
 
     /**
      * @property $DATABASE_CHARSET string
      * set charset for database names
      */
-    public static $DATABASE_CHARSET = "utf8";
+    public $DATABASE_CHARSET = "utf8";
 
     /**
      * @property $DEV bool
      * set false when you upload your project to final server
      */
-    public static $DEV = true;
+    public $DEV = true;
 
     /**
      * @property $DBEXT bool
      * on | off mysql extensions and all class. Inflicts DelegateQuery, QueryBuilder, CurrentDB, Connections, Database, DbDict, Models and Migrate class
      */
-    public static $DBEXT = true;
+    public $DBEXT = true;
 
     /**
      * @property $USE_PREPARED_STMT bool
@@ -51,7 +53,7 @@ class WingedConfig extends \stdClass
      * view more of prepared statements in
      * <your_domain_name>/winged/what_is_prepared_statement
      */
-    public static $USE_PREPARED_STMT = NO_USE_PREPARED_STMT;
+    public $USE_PREPARED_STMT = NO_USE_PREPARED_STMT;
 
     /**
      * @property $DB_DRIVER string
@@ -60,69 +62,69 @@ class WingedConfig extends \stdClass
      * only mysql will be available for use. To see the availability of classes and functions of your server,
      * go to <your_domain_name>/winged/available#database
      */
-    public static $DB_DRIVER = DB_DRIVER_MYSQL;
+    public $DB_DRIVER = DB_DRIVER_MYSQL;
 
     /**
      * @property $STD_DB_CLASS string
      * defines which class will be used for the interaction between PHP and the database
      */
-    public static $STD_DB_CLASS = IS_MYSQLI;
+    public $STD_DB_CLASS = IS_MYSQLI;
 
     /**
      * @property $STANDARD string
      * your main and default route for rewrite url
      */
-    public static $STANDARD = "news";
+    public $STANDARD = "home";
 
     /**
      * @property $STANDARD_CONTROLLER string
      * defines the name of your primary controller when no name for controllador was found in the url
      */
-    public static $STANDARD_CONTROLLER = "news";
+    public $STANDARD_CONTROLLER = "home";
 
     /**
      * @property $CONTROLLER_DEBUG bool
      * on | off erros and warning of main Controller class
      */
-    public static $CONTROLLER_DEBUG = true;
+    public $CONTROLLER_DEBUG = true;
 
     /**
      * @property $PARENT_FOLDER_MVC bool
      * on | off search for better structure MVC folder within folders defined by URL
      * !IMPORTANT: true is recommended, because it enhances the organization of your project
      */
-    public static $PARENT_FOLDER_MVC = true;
+    public $PARENT_FOLDER_MVC = true;
 
     /**
      * @property $HEAD_CONTENT_PATH string
      * defines path to include in every page called in any Controller by method renderHtml()
      * this option can be rewrited with method rewriteHeadContentPath() of any Controller
      */
-    public static $HEAD_CONTENT_PATH = null;
+    public $HEAD_CONTENT_PATH = null;
 
     /**
      * @property $HOST string
      * defines default server name for mysql connection
      */
-    public static $HOST = "localhost";
+    public $HOST = "localhost";
 
     /**
      * @property $USER string
      * default user name for mysql connection
      */
-    public static $USER = "root";
+    public $USER = "root";
 
     /**
      * @property $DBNAME string
      * default database name for mysql connection
      */
-    public static $DBNAME = "beesystem";
+    public $DBNAME = "osc";
 
     /**
      * @property $PASSWORD string
      * default password for mysql connection
      */
-    public static $PASSWORD = "";
+    public $PASSWORD = "";
 
     /**
      * @property $ROUTER string
@@ -132,57 +134,43 @@ class WingedConfig extends \stdClass
      * constant ROOT_ROUTES_PAGE_NAME search folder with name "routes" in level of main "index.php" and search file "<page from url>.php" inside this folder
      * constant ROOT_ROUTES_ROUTE_PHP search folder with name "routes" in level of main "index.php" and search file "routes.php" inside this folder
      */
-    public static $ROUTER = PARENT_ROUTES_ROUTE_PHP;
+    public $ROUTER = PARENT_ROUTES_ROUTE_PHP;
 
     /**
      * @property $FORCE_NOTFOUND bool
      * ignore errors on the controllers and the routes, always forcing the presentation of the page not found
      */
-    public static $FORCE_NOTFOUND = true;
+    public $FORCE_NOTFOUND = true;
 
     /**
      * @property $TIMEZONE string
      * sets the time zone used in the entire system
      */
-    public static $TIMEZONE = "America/Sao_Paulo";
+    public $TIMEZONE = "America/Sao_Paulo";
 
     /**
      * @property $NOTFOUND string
      * defines the path to the page file not found
      */
-    public static $NOTFOUND = "./404.php";
-
-    /**
-     * @var bool
-     * if this property is true, the default response for 404 not found got be with a route system
-     */
-    public static $USE_404_WITH_ROUTES = true;
+    public $NOTFOUND = "./404.php";
 
     /**
      * @property $DEBUG bool
      * on | off display errors
      */
-    public static $DEBUG = true;
-
-    /**
-     * @property $NOT_WINGED bool
-     * warning: this option able a not winged view mode.
-     * this option read first dir pure-html in root of your project if they exists and if file exists inside it
-     * ignores read for controller, restful and rewrite class if file in this dir found
-     */
-    public static $NOT_WINGED = true;
+    public $DEBUG = true;
 
     /**
      * @property $INTERNAL_ENCODING array
      * this property defines the internal enconding of PHP, it uses [mb] lib
      */
-    public static $INTERNAL_ENCODING = "UTF-8";
+    public $INTERNAL_ENCODING = "UTF-8";
 
     /**
      * @property $OUTPUT_ENCODING array
      * this property defines the html output enconding, it uses [mb] lib
      */
-    public static $OUTPUT_ENCODING = "UTF-8";
+    public $OUTPUT_ENCODING = "UTF-8";
 
     /**
      * @var $USE_UNICID_ON_INCLUDE_ASSETS bool | array
@@ -192,7 +180,7 @@ class WingedConfig extends \stdClass
      * so that your project loads faster and offers a better end-user experience.
      * you can add what the targets will be, adding the name of the tags within the array
      */
-    public static $USE_UNICID_ON_INCLUDE_ASSETS = ['img', 'script', 'link', 'source'];
+    public $USE_UNICID_ON_INCLUDE_ASSETS = ['img', 'script', 'link', 'source'];
 
 
     /**
@@ -203,15 +191,32 @@ class WingedConfig extends \stdClass
      * if an update can be detected in any file in queue the minified resulted file before change was deleted and other as created
      * on end of project turn this to false
      */
-    public static $AUTO_MINIFY = 20;
+    public $AUTO_MINIFY = 1;
 
+    /**
+     * gzencode offers an suporte to html, json, js and css files
+     * it reduces size of requests
+     */
+    public $USE_GZENCODE = true;
+
+    /**
+     * @var $FORCE_WWW bool
+     * if www not found in URL, Winged make an redirect with same URL with www
+     */
+    public $FORCE_WWW = true;
+
+    /**
+     * @var $FORCE_HTTPS bool
+     * if https not found in URL, Winged make an redirect with same URL with https
+     */
+    public $FORCE_HTTPS = true;
 
     /**
      * @property $INCLUDES array
      * it includes all paths that are within that variable if they exist and are a valid php file
      * util if you have two classes with same name, and autoload can't load these classes
      */
-    public static $INCLUDES = [
+    public $INCLUDES = [
 
     ];
 }
